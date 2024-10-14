49c6b17a59060c4f08a29f78815a88058fe7de11
lanariel.silver <lanariel.silver@gmail.com>
2024-10-15 01:43:53 +0200

    Created StartPage and Model

---
+++ RoomInspectionApplication/RoomInspectionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomInspectionApplication
+{
+    internal class RoomInspectionModel
+    {
+        DateTime time = DateTime.Now;
+
+        public static RoomInspectionModel LoadFromFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
